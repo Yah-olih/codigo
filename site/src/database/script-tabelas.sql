470047e31db3f0f67f7fb718d0dc13bf35cfff2a
Yah-olih <yara.santana@sptech.school>
2022-11-21 20:30:30 -0300

    2 parte

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -2,6 +2,19 @@ create database criminalMids;
 use criminalMids;
 drop database criminalmids;
 
+CREATE TABLE usuario (
+  idUsuario INT PRIMARY KEY auto_increment,
+  nome VARCHAR(45),
+  email VARCHAR(100),
+  senha VARCHAR(20),
+  fkpesonagem int,
+  foreign key (fkpesonagem) references pesonagem(idpesonagem));
+  
+insert into usuario (nome, email,senha) values
+('teste', 'teste@gmail.com',123456789);
+
+select * from usuario;
+
 create table pesonagem (
 idpesonagem int primary key auto_increment,
 AaronHotcher varchar(45),
@@ -14,15 +27,14 @@ JenniferJareau  varchar(45),
 JasinGideon  varchar(45),
 KateCallahan  varchar(45));
 
-CREATE TABLE usuario (
-  idusuario INT PRIMARY KEY auto_increment,
-  nome VARCHAR(45),
-  email VARCHAR(100),
-  senha VARCHAR(20),
-  fkpesonagem int,
-  foreign key (fkpesonagem) references pesonagem(idpesonagem));
-  
-insert into usuario (nome, email,senha) values
-('teste', 'teste@gmail.com',123456789);
+create table aviso (
+idAviso int primary key auto_increment,
+titulo varchar(45),
+descricao varchar(250),
+fk_usuario int,
+foreign key (fk_usuario) references usuario(idUsuario));
 
-select * from usuario;
+insert into aviso (titulo,descricao,fk_usuario) values
+('titulo', 'comentario', 1);
+
+select * from aviso;
